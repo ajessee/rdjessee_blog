a830f16bed9c330dbaab5f7b17be70905173d27e
Andre Jesse <andre.isaac.jessee@gmail.com>
2016-03-14 21:20:02 -0400

    finish adding user class

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
 
   get 'about' => 'welcome#about'
 
-  get 'users/new'
+  get 'signup' => 'users#new'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
