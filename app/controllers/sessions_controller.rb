ad21db21ce212dcf17d1863fcf3332b122666898
ajessee <andre.isaac.jessee@gmail.com>
2017-05-04 20:42:02 -0400

    fix problem with edit story button, attempt 2

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -14,7 +14,7 @@ class SessionsController < ApplicationController
         message  = "Account not activated. "
         message += "Check your email for the activation link."
         flash[:warning] = message
-        redirect_to root_url
+        redirect_to "/#intro"
       end
     else
       flash.now[:danger] = 'Invalid email/password combination'
