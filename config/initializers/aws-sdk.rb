6d151cd8ed45b051a20a716d97e289de71e11747
Andre Jessee <andre.isaac.jessee@gmail.com>
2020-03-29 21:41:48 -0400

    i think i'm close - was passing bucket information to region for aws mail

--- config/initializers/aws-sdk.rb
+++ config/initializers/aws-sdk.rb
@@ -4,5 +4,3 @@ Aws.config.update({
   region: 'us-east-1',
   credentials: Aws::Credentials.new(Rails.application.credentials.dig(:aws, :access_key_id), Rails.application.credentials.dig(:aws, :secret_access_key))
 })
-
-Aws.config[:credentials] = Aws::Credentials.new(Rails.application.credentials.dig(:aws, :access_key_id), Rails.application.credentials.dig(:aws, :secret_access_key))
--- config/initializers/simple_email_service.rb
+++ config/initializers/simple_email_service.rb
@@ -1,4 +1,4 @@
 if Rails.env.production?
   aws_credentials = Aws::Credentials.new(Rails.application.credentials.dig(:aws, :access_key_id), Rails.application.credentials.dig(:aws, :secret_access_key))
-  Aws::Rails.add_action_mailer_delivery_method(:aws_sdk, credentials: aws_credentials, region: Rails.application.credentials.dig(:aws, :bucket))
+  Aws::Rails.add_action_mailer_delivery_method(:aws_sdk, credentials: aws_credentials, region: Rails.application.credentials.dig(:aws, :region))
 end
