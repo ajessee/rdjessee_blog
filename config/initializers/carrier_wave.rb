a109454de1ec5df93598682e21fe9e0cbd843d75
Andre Jessee <andre.isaac.jessee@gmail.com>
2020-03-30 19:25:01 -0400

    remove all reference to carrierwave2

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -1,32 +1,32 @@
-if Rails.env.production? 
-  CarrierWave.configure do |config|
-    config.storage    = :aws
-    config.aws_bucket = 'andre-pictures'
-    config.aws_acl    = 'public-read'
-    # Optionally define an asset host for configurations that are fronted by a
-    # content host, such as CloudFront.
-    # config.asset_host = 'http://example.com'
+# if Rails.env.production? 
+#   CarrierWave.configure do |config|
+#     config.storage    = :aws
+#     config.aws_bucket = 'andre-pictures'
+#     config.aws_acl    = 'public-read'
+#     # Optionally define an asset host for configurations that are fronted by a
+#     # content host, such as CloudFront.
+#     # config.asset_host = 'http://example.com'
 
-    # The maximum period for authenticated_urls is only 7 days.
-    # config.aws_authenticated_url_expiration = 60 * 60 * 24 * 7
+#     # The maximum period for authenticated_urls is only 7 days.
+#     # config.aws_authenticated_url_expiration = 60 * 60 * 24 * 7
 
-    # Set custom options such as cache control to leverage browser caching
-    # config.aws_attributes = {
-      # expires: 1.week.from_now.httpdate,
-      # cache_control: 'max-age=604800'
-    # }
+#     # Set custom options such as cache control to leverage browser caching
+#     # config.aws_attributes = {
+#       # expires: 1.week.from_now.httpdate,
+#       # cache_control: 'max-age=604800'
+#     # }
 
-    config.aws_credentials = {
-      access_key_id:     Rails.application.credentials.dig(:aws, :access_key_id),
-      secret_access_key: Rails.application.credentials.dig(:aws, :secret_access_key),
-      region:            Rails.application.credentials.dig(:aws, :region) # Required
-    }
+#     config.aws_credentials = {
+#       access_key_id:     Rails.application.credentials.dig(:aws, :access_key_id),
+#       secret_access_key: Rails.application.credentials.dig(:aws, :secret_access_key),
+#       region:            Rails.application.credentials.dig(:aws, :region) # Required
+#     }
 
-    # Optional: Signing of download urls, e.g. for serving private content through
-    # CloudFront. Be sure you have the `cloudfront-signer` gem installed and
-    # configured:
-    # config.aws_signer = -> (unsigned_url, options) do
-    #   Aws::CF::Signer.sign_url(unsigned_url, options)
-    # end
-  end
-end
+#     # Optional: Signing of download urls, e.g. for serving private content through
+#     # CloudFront. Be sure you have the `cloudfront-signer` gem installed and
+#     # configured:
+#     # config.aws_signer = -> (unsigned_url, options) do
+#     #   Aws::CF::Signer.sign_url(unsigned_url, options)
+#     # end
+#   end
+# end
