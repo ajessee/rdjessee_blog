5b9e0c3382b260f60d0aeb9129eeff624de690f9
Andre Jessee <andre.isaac.jessee@gmail.com>
2020-03-29 22:07:40 -0400

    running out of ideas

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -1,7 +1,7 @@
 if Rails.env.production? 
   CarrierWave.configure do |config|
     config.storage    = :aws
-    config.aws_bucket = Rails.application.credentials.dig(:aws, :bucket)
+    config.aws_bucket = ENV.fetch('S3_BUCKET')
     config.aws_acl    = 'public-read'
     # Optionally define an asset host for configurations that are fronted by a
     # content host, such as CloudFront.
@@ -19,8 +19,7 @@ if Rails.env.production?
     config.aws_credentials = {
       access_key_id:     Rails.application.credentials.dig(:aws, :access_key_id),
       secret_access_key: Rails.application.credentials.dig(:aws, :secret_access_key),
-      region:            Rails.application.credentials.dig(:aws, :region), # Required
-      bucket: Rails.application.credentials.dig(:aws, :bucket)
+      region:            Rails.application.credentials.dig(:aws, :region) # Required
     }
 
     # Optional: Signing of download urls, e.g. for serving private content through
--- lib/tasks/migrate_images.rake
+++ lib/tasks/migrate_images.rake
@@ -4,9 +4,6 @@
 # lib/tasks/migrate_picture.rake
 
 task :migrate_picture => :environment do
-
-    
-
     def migrate_attachment!(klass:, attachment_attribute:, carrierwave_uploader:, active_storage_column: attachment_attribute)
         klass.find_each do |item|
             next unless item.send(attachment_attribute).present?
