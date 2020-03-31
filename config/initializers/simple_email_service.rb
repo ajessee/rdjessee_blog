bcd5755966f37a3268fec597b4e8adffe0ea5095
Andre Jessee <andre.isaac.jessee@gmail.com>
2020-03-31 01:11:10 -0400

    not goona keep doing this2

--- config/initializers/simple_email_service.rb
+++ config/initializers/simple_email_service.rb
@@ -1,4 +1,4 @@
 if Rails.env.production?
-  aws_credentials = Aws::Credentials.new(Rails.application.credentials.dig(:aws, :access_key_id), Rails.application.credentials.dig(:aws, :secret_access_key))
-  Aws::Rails.add_action_mailer_delivery_method(:aws_sdk, credentials: aws_credentials, region: Rails.application.credentials.dig(:aws, :region))
+  aws_credentials = Aws::Credentials.new(ENV['AWS_ACCESS_KEY_ID'], ENV['AWS_SECRET_ACCESS_KEY'])
+  Aws::Rails.add_action_mailer_delivery_method(:aws_sdk, credentials: aws_credentials, region: ENV['AWS_REGION'])
 end
