5679ff24e0f55b73778307b9095c7800e2be6395
Andre Jessee <andre.isaac.jessee@gmail.com>
2020-04-05 18:23:36 -0400

    prepend www to host for email service in prod

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -61,9 +61,10 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "rdjessee_blog_#{Rails.env}"
 
   # Email Config
+  config.action_mailer.perform_caching = false
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  host = 'ralphdonaldjessee.com'
+  host = 'www.ralphdonaldjessee.com'
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
     address: 'smtp.sendgrid.net',
@@ -74,10 +75,6 @@ Rails.application.configure do
     domain: 'heroku.com',
     enable_starttls_auto: true
   }
-
-  # Ignore bad email addresses and do not raise email delivery errors.
-  # Set this to true and configure the email server for immediate delivery to raise delivery errors.
-  # config.action_mailer.raise_delivery_errors = false
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
