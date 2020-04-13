a721e5c7608eff1c446b604e0e10b0f0bddd41a6
Andre Jessee <andre.isaac.jessee@gmail.com>
2020-04-12 22:40:58 -0400

    make elastic config env dependent

--- config/initializers/elasticsearch.rb
+++ config/initializers/elasticsearch.rb
@@ -1,6 +1,6 @@
 # config/initializers/elasticsearch.rb
 
-if File.exists?("config/elasticsearch.yml")
+if File.exists?("config/elasticsearch.yml") && Rails.env.development? || Rails.env.test?
     config = YAML.load_file("config/elasticsearch.yml")[Rails.env].symbolize_keys
     Elasticsearch::Model.client = Elasticsearch::Client.new(config)
  end
