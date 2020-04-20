5e386e8ee6d71676ed69b05af61f4c48c115c73a
Andre Jessee <andre.isaac.jessee@gmail.com>
2020-04-19 23:15:48 -0400

    add conversion method for all recordings to be converted to m4a

--- app/models/recording.rb
+++ app/models/recording.rb
@@ -2,6 +2,24 @@ class Recording < ApplicationRecord
   belongs_to :recorder, class_name: 'User', foreign_key: :user_id
   belongs_to :recordable, polymorphic: true
   has_one_attached :audio_file
+  after_commit :process_audio!
 
-  def convert_to_mp4
+  def process_audio!
+    if audio_file.attached? && audio_file.blob.content_type != 'audio/mp4'
+
+      orig_audio_tmpfile = self.attachment_changes['audio_file'].attachable.path.to_s
+      mp4_audio_tmpfile = "#{Rails.root}/tmp/#{audio_file.blob.key}_#{audio_file.blob.filename.base}.m4a"
+
+      system("ffmpeg -i #{orig_audio_tmpfile} -c:a aac -b:a 192k #{mp4_audio_tmpfile}")
+  
+      self.audio_file.attach(
+        io: File.open(mp4_audio_tmpfile),
+        filename: "#{audio_file.blob.filename.base}.m4a",
+        content_type: 'audio/mp4'
+      )
+  
+      File.delete(mp4_audio_tmpfile)
+    end
+  end
+
 end
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -29,7 +29,7 @@ Rails.application.configure do
   end
 
   # Store uploaded files on the local file system (see config/storage.yml for options).
-  config.active_storage.service = :local
+  config.active_storage.service = :amazon
 
   # Email in development
   config.action_mailer.raise_delivery_errors = true
