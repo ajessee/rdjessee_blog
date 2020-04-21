c5aff88ddc2969e0055301616f6d0b7c7da75bf2
Andre Jessee <andre.isaac.jessee@gmail.com>
2020-04-20 22:59:28 -0400

    sanitize audio file name before save

--- app/models/recording.rb
+++ app/models/recording.rb
@@ -3,6 +3,14 @@ class Recording < ApplicationRecord
   belongs_to :recordable, polymorphic: true
   has_one_attached :audio_file
   after_commit :process_audio!
+  after_create :set_filename
+
+  def set_filename
+    if self.audio_file.attached?
+      title = Story.find(self.recordable_id).title
+      self.audio_file.blob.update(filename: "#{self.audio_file.filename.base.gsub(/[^0-9A-Za-z]/, '')}#{self.audio_file.filename.extension}")
+    end
+  end
 
   def process_audio!
     if audio_file.attached? && audio_file.blob.content_type != 'audio/mp4'
