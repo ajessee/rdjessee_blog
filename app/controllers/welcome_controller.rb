ca090135b664a12c7999201a49d5145b8f38c13c
Andre Jessee <andre.isaac.jessee@gmail.com>
2020-05-06 00:58:54 -0400

    update number of recordings to show on preview and full page

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -5,7 +5,7 @@ class WelcomeController < ApplicationController
     @pictures = Picture.order("RANDOM()").limit(3)
     @videos = Video.order("RANDOM()").limit(3)
     @stories_with_recordings = Story.joins(:recordings).order("RANDOM()").limit(3)
-    @user_recordings = Recording.where(recordable_type: "User").order("RANDOM()").limit(5)
+    @user_recordings = Recording.where(recordable_type: "User").order("RANDOM()").limit(4)
     @guestbook_comments = Comment.where(commentable_type: "User")
   end
 
